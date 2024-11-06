c18069d6783fb3125344433b44506686e3b93e24
Wellinghton O. <wellinghtonbatistahotmail@gmail.com>
2024-11-06 13:26:38 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    console.log('entrou aqui!')
+    res.send('Olá Mundo!')
+})
+
 app.listen(PORT, () => {
     console.log(`App online na porta ${PORT}`)
 })
